8a4fab43ffd0e9d9538b51b1d5977c25abbc33d4
santa2021 <76956050+santa2021@users.noreply.github.com>
2023-08-18 03:48:02 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -227,24 +227,21 @@ EOF
 
 generate_nezha() {
   cat > nezha.sh << EOF
+
 #!/usr/bin/env bash
 
-# 哪吒的三个参数
 NEZHA_SERVER=${NEZHA_SERVER}
 NEZHA_PORT=${NEZHA_PORT}
 NEZHA_KEY=${NEZHA_KEY}
 
-# 检测是否已运行
 check_run() {
     [[ \$(pidof nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
 }
 
-# 三个变量不全则不安装哪吒客户端
 check_variable() {
     [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
 }
 
-# 下载最新版本 Nezha Agent
 download_agent() {
     if [ ! -e nezha-agent ]; then
         URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
@@ -253,7 +250,7 @@ download_agent() {
     fi
 }
 
-# 运行客户端
+
 run() {
     [ -e nezha-agent ] && chmod +x nezha-agent && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY}
 }
